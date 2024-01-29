9427e65da9abe533e48b7ffd941597b472222a98
slam814 <abdussalam6111997@gmail.com>
2024-01-29 20:51:20 +0600

    adde some file

---
+++ function.js
@@ -0,0 +1,17 @@
+document.getElementById('apply-btn').addEventListener('click', function(){
+    const inputFildElement = document.getElementById('input-fild');
+    const inputFildString = inputFildElement.value;
+    const inputFildPrice = parseFloat(inputFildString)
+    // discount 
+    const discountElement = document.getElementById('discount-price');
+    const discountFildString = discountElement.innerText;
+    const discountFildPrice = parseFloat(discountFildString);
+
+    const discount = (discountFildPrice / 100) * inputFildPrice;
+
+    const discountPrice = inputFildPrice - discount;
+
+    // set price 
+    const priceElement = document.getElementById('product-price');
+    priceElement.innerText = discountPrice;
+})
