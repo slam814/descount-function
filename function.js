ac322e2baf7fd5c0f4df70ff12d6291ff736521b
slam814 <abdussalam6111997@gmail.com>
2024-01-29 21:21:34 +0600

    i change the file

--- function.js
+++ function.js
@@ -1,11 +1,25 @@
 document.getElementById('apply-btn').addEventListener('click', function(){
     const inputFildElement = document.getElementById('input-fild');
     const inputFildString = inputFildElement.value;
-    const inputFildPrice = parseFloat(inputFildString)
+    const inputFildPrice = parseFloat(inputFildString);
+    
+    // worning 
+    const worningMessege = document.getElementById('worning-message');
+    if(isNaN(inputFildPrice)){
+        worningMessege.innerText = 'Please input Valid Number!';
+     
+        return ;
+    }else{
+        worningMessege.innerText = '';
+        inputFildElement.value = '';
+    }
+
     // discount 
     const discountElement = document.getElementById('discount-price');
     const discountFildString = discountElement.innerText;
     const discountFildPrice = parseFloat(discountFildString);
+
+    
 
     const discount = (discountFildPrice / 100) * inputFildPrice;
 
@@ -13,5 +27,5 @@ document.getElementById('apply-btn').addEventListener('click', function(){
 
     // set price 
     const priceElement = document.getElementById('product-price');
-    priceElement.innerText = discountPrice;
+    priceElement.innerText = discountPrice.toFixed(2);
 })
